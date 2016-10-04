eb3d6f20cdb483956d34c7b8b69964a37ff37e1a
Noah D. Brenowitz <nbren12@gmail.com>
2016-10-04 15:07:37 -0400

    add documentation to run_mc.py

--- examples/python/run_mc.py
+++ examples/python/run_mc.py
@@ -1,7 +1,22 @@
+"""Run python multicloud model
+
+Usage:
+    run_mc.py [-r <restart_file>]
+
+Options:
+    -r --restart   use restart file to initialize run
+"""
+import shutil
 import sys
 
 sys.path.insert(0, "../../")
 from python.swe.multicloud import main
 
 if __name__ == '__main__':
+    from docopt import docopt
+    args = docopt(__doc__)
+
+    if args['--restart']:
+        shutil.copy(args['<restart_file>'], 'restart.pkl')
+
     main()
