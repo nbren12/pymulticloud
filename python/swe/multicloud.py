956f13eb47f838ac31c54bbcf356735267129543
Noah D. Brenowitz <nbren12@gmail.com>
2016-10-06 15:23:16 -0400

    encapsulate solver in class

--- python/swe/multicloud.py
+++ python/swe/multicloud.py
@@ -23,100 +23,118 @@ logger = logging.getLogger(__file__)
 
 L = 3
 
-variables = ['q', 'teb', 'hs', 'tebst', 'fc', 'fd', 'fs']
-variable_idxs = dict(zip(variables, itertools.count(2 * L)))
-variable_idxs['u'] = slice(0, 2 * L, 2)
-variable_idxs['t'] = slice(1, 2 * L, 2)
+class MulticloudModel(object):
+    L = 3
+
+    def __init__(self):
+        variables = ['q', 'teb', 'hs', 'tebst', 'fc', 'fd', 'fs']
+        variable_idxs = dict(zip(variables, itertools.count(2 * L)))
+        variable_idxs['u'] = slice(0, 2 * L, 2)
+        variable_idxs['t'] = slice(1, 2 * L, 2)
+
+        self.variable_idxs = variable_idxs
+        self.variables = variables
 
 
-def f(q, alpha_tld=0.1, lmd_tld=0.8, q_tld=0.9, nonlinear=0.0):
-    """Flux function for multicloud model"""
 
-    u = q[variable_idxs['u'], ...]
-    T = q[variable_idxs['t'], ...]
-    moist = q[variable_idxs['q'], ...]
+    def _f(self, q, alpha_tld=0.1, lmd_tld=0.8, q_tld=0.9, nonlinear=0.0):
+        """Flux function for multicloud model"""
+        variable_idxs = self.variable_idxs
 
-    fq = np.empty_like(q)
-    f2m(q, fq=fq, nonlin=nonlinear)
+        u = q[variable_idxs['u'], ...]
+        T = q[variable_idxs['t'], ...]
+        moist = q[variable_idxs['q'], ...]
 
-    # moisture equation
-    fq[variable_idxs['q'], ...] = q_tld * (u[1] + lmd_tld * u[2]) \
-                                  + moist * (u[1] + alpha_tld * u[2])
+        fq = np.empty_like(q)
+        f2m(q, fq=fq, nonlin=nonlinear)
 
-    return fq
+        # moisture equation
+        fq[variable_idxs['q'], ...] = q_tld * (u[1] + lmd_tld * u[2]) \
+                                    + moist * (u[1] + alpha_tld * u[2])
 
-
-def onestep(soln, time, dt, dx, nonlinear=1.0):
-    """Perform a single time step of the multicloud model"""
-    from functools import partial
-
-    # hyperbolic terms
-    periodic_bc(soln)
-    f_partial = partial(f, nonlinear=nonlinear)
-    soln[:2 * L + 1, ...] = central_scheme(f_partial, soln[:2 * L + 1, ...],
-                                           dx, dt)
-
-    # multicloud model step
-    mc.multicloud_rhs(soln[variable_idxs['fc']], soln[variable_idxs['fd']],
-                      soln[variable_idxs['fs']], soln[variable_idxs['u']][1],
-                      soln[variable_idxs['u']][2], soln[variable_idxs['t']][1],
-                      soln[variable_idxs['t']][2], soln[variable_idxs['teb']],
-                      soln[variable_idxs['q']], soln[variable_idxs['hs']], dt,
-                      dx, time, soln[variable_idxs['tebst']])
-
-    return soln
+        return fq
 
 
-def init_mc(n=1000, dx=50 / 1500):
-    import numpy as np
-    neq = 2 * L + len(variables)
-    soln = np.zeros((neq, n))
+    def onestep(self, soln, time, dt, dx, nonlinear=1.0):
+        """Perform a single time step of the multicloud model"""
+        from functools import partial
+        variable_idxs = self.variable_idxs
 
-    fceq, fdeq, fseq = mc.equilibrium_fractions()
+        # hyperbolic terms
+        periodic_bc(soln)
+        f_partial = partial(self._f, nonlinear=nonlinear)
+        soln[:2 * L + 1, ...] = central_scheme(f_partial, soln[:2 * L + 1, ...],
+                                            dx, dt)
 
-    soln[variable_idxs['fc']] = fceq
-    soln[variable_idxs['fd']] = fdeq
-    soln[variable_idxs['fs']] = fseq
+        # multicloud model step
+        mc.multicloud_rhs(soln[variable_idxs['fc']], soln[variable_idxs['fd']],
+                        soln[variable_idxs['fs']], soln[variable_idxs['u']][1],
+                        soln[variable_idxs['u']][2], soln[variable_idxs['t']][1],
+                        soln[variable_idxs['t']][2], soln[variable_idxs['teb']],
+                        soln[variable_idxs['q']], soln[variable_idxs['hs']], dt,
+                        dx, time, soln[variable_idxs['tebst']])
 
-    # initialize temperature field with small random perturbation
-    soln[variable_idxs['t'], ...][0] = np.random.randn(n) * .01
+        return soln
 
-    return soln, dx
+    @property
+    def neq(self):
+        return 2 * self.L + len(self.variables)
 
 
-def init_mc_from_file(fn):
-    neq = 2 * L + len(variables)
-    icdata = np.load(fn)['arr_0'][-1]
-    n = icdata['teb'].shape[0] + 4
-    soln = np.zeros((neq, n))
+    def init_mc(self, n=1000, dx=50 / 1500):
+        variable_idxs = self.variable_idxs
 
-    for key in variable_idxs:
-        soln[variable_idxs[key]][..., 2:-2] = icdata[key]
+        soln = np.zeros((self.neq, n))
 
-    return soln
+        fceq, fdeq, fseq = mc.equilibrium_fractions()
+
+        soln[variable_idxs['fc']] = fceq
+        soln[variable_idxs['fd']] = fdeq
+        soln[variable_idxs['fs']] = fseq
+
+        # initialize temperature field with small random perturbation
+        soln[variable_idxs['t'], ...][0] = np.random.randn(n) * .01
+
+        return soln, dx
+
+
+    def init_mc_from_file(self, fn):
+
+        variable_idxs = self.variable_idxs
+        neq = self.neq
+        icdata = np.load(fn)['arr_0'][-1]
+        n = icdata['teb'].shape[0] + 4
+        soln = np.zeros((neq, n))
+
+        for key in variable_idxs:
+            soln[variable_idxs[key]][..., 2:-2] = icdata[key]
+
+        return soln
+
+    def record_array_soln(self, soln, t):
+        """Create record array for solution"""
+        variables = self.variables
+        variable_idxs = self.variable_idxs
+
+        soln = unghosted(soln)
+        n = soln.shape[-1]
+
+        variables_dtype = [(k, np.float64, (n,)) for k in variables] \
+                        + [(k, np.float64, (L, n)) for k in ['u', 't']]
+        mydt = np.dtype([('time', np.float64)] + variables_dtype)
+        arr = np.zeros(1, dtype=mydt)
+
+        for k, v in variable_idxs.items():
+            arr[k] = soln[v]
+
+        arr['time'] = t
+
+        return arr
 
 
 def unghosted(q):
     return q[:, 2:-2]
 
-
-def record_array_soln(soln, t):
-    """Create record array for solution"""
-
-    soln = unghosted(soln)
-    n = soln.shape[-1]
-
-    variables_dtype = [(k, np.float64, (n,)) for k in variables] \
-                      + [(k, np.float64, (L, n)) for k in ['u', 't']]
-    mydt = np.dtype([('time', np.float64)] + variables_dtype)
-    arr = np.zeros(1, dtype=mydt)
-
-    for k, v in variable_idxs.items():
-        arr[k] = soln[v]
-
-    arr['time'] = t
-
-    return arr
 
 
 def save_restart_file(name, soln, t, dx):
@@ -134,22 +152,26 @@ def load_restart_file(name):
     return arr
 
 
-def main(run_duration=100):
+def main(run_duration=100, solver=None):
     """Runs multicloud model
 
     TODO This file is too complicated needs to be refactored, and the IO needs
     to be rethought
     """
-    soln, dx = init_mc()
     t_start = 0.0
 
     logger.info("Starting run with duration={0}".format(run_duration))
+
+    if solver is None:
+        solver = MulticloudModel()
+
+    soln, dx = solver.init_mc()
 
     if os.path.exists('restart.pkl'):
         soln, t_start, dx = load_restart_file("restart.pkl")
         logger.info("Loading restart file at t={0}".format(t_start))
     elif os.path.exists('ic.npz'):
-        soln = init_mc_from_file("ic.npz")
+        soln = solver.init_mc_from_file("ic.npz")
 
     dt = dx * .1
     t_end = t_start + run_duration
@@ -159,7 +181,7 @@ def main(run_duration=100):
 
     # allocate output buffer
     nbuf = 10
-    arr = record_array_soln(soln, t_start)
+    arr = solver.record_array_soln(soln, t_start)
     output = np.zeros(nbuf, dtype=arr.dtype)
 
     # include initial data
@@ -171,11 +193,11 @@ def main(run_duration=100):
     if not os.path.isdir(datadir):
         os.mkdir(datadir)
 
-    for t, soln in steps(onestep, soln, dt, (t_start, t_end), dx):
+    for t, soln in steps(solver.onestep, soln, dt, (t_start, t_end), dx):
 
         if t > t_out:
             logger.info("Storing output data at t={0}".format(t))
-            output[i_out] = record_array_soln(soln, t)
+            output[i_out] = solver.record_array_soln(soln, t)
             t_out += dt_out
             i_out += 1
 
