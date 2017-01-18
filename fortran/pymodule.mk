5437618a1d3c9650d412a1ce2976178114410658
Noah D. Brenowitz <nbren12@gmail.com>
2017-01-17 19:50:45 -0500

    fix compilation

--- fortran/pymodule.mk
+++ fortran/pymodule.mk
@@ -1,11 +1,20 @@
 FC= gfortran
 FFLAGS = -O3
 
-all: mc.pyf mcf90.o
-	f2py -c mc.pyf mc.f90 mcf90.o
+all: mc.pyf multicloud_mod.o 
+	f2py -c mc.pyf mc.f90 multicloud_mod.o util.o param_mod.o duni.f ext/minpack/*.f
 mc.pyf: mc.f90
 	f2py -h mc.pyf -m mc mc.f90 --overwrite-signature
 .PHONY: mc.pyf
 
-mcf90.o: multicloud_mod.f90 param_mod.f90 duni.f
-	$(FC) $(FFLAGS) multicloud_mod.f90 param_mod.f90 duni.f ext/minpack/*.f
+multicloud_mod.o : multicloud_mod.f90 util.o param_mod.o
+	$(FC) $(FFLAGS) -c $<
+
+%.o : %.f90
+	$(FC) $(FFLAGS) -c $^
+
+#mcf90.o: multicloud_mod.o param_mod.o util.o duni.f
+#	$(FC) $(FFLAGS) -c -o $@ multicloud_mod.o util.o param_mod.o duni.f ext/minpack/*.f
+
+clean: 
+	rm -f *.o *.mod
