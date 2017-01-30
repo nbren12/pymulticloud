d21e9f300b9482da18fc3cd99cad7a6bf46f1800
Noah D. Brenowitz <nbren12@gmail.com>
2017-01-30 15:07:49 -0500

    add -fPIC flag to compliation

--- fortran/pymodule.mk
+++ fortran/pymodule.mk
@@ -2,16 +2,16 @@ FC= gfortran
 FFLAGS = -O3
 
 all: mc.pyf multicloud_mod.o 
-	f2py -c mc.pyf mc.f90 multicloud_mod.o util.o param_mod.o duni.f ext/minpack/*.f
+	FC=$(FC) f2py -c mc.pyf mc.f90 multicloud_mod.o util.o param_mod.o duni.f ext/minpack/*.f
 mc.pyf: mc.f90
-	f2py -h mc.pyf -m mc mc.f90 --overwrite-signature
+	FC=$(FC) f2py -h mc.pyf -m mc mc.f90 --overwrite-signature
 .PHONY: mc.pyf
 
 multicloud_mod.o : multicloud_mod.f90 util.o param_mod.o
-	$(FC) $(FFLAGS) -c $<
+	$(FC) $(FFLAGS) -fPIC -c $<
 
 %.o : %.f90
-	$(FC) $(FFLAGS) -c $^
+	$(FC) $(FFLAGS) -fPIC -c $^
 
 #mcf90.o: multicloud_mod.o param_mod.o util.o duni.f
 #	$(FC) $(FFLAGS) -c -o $@ multicloud_mod.o util.o param_mod.o duni.f ext/minpack/*.f
